265ed36de61d4f92fd5f34010e47ac698c8e6ac4
Mavdavis <ekwesimarvelous@gmail.com>
2025-09-16 13:46:39 +0100

    wip

---
+++ src/components/Layout/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="flex">
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="/">
+                        <img src="" alt="Logo">
+                    </a>
+                </div>
+                <div class="navbar-menu">
+                    <div class="navbar-start">
+                        <router-link class="navbar-item" to="/">Home</router-link>
+                        <router-link class="navbar-item" to="/about">About</router-link>
+                        <router-link class="navbar-item" to="/contact">Contact</router-link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+<script setup>
+</script>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -2,8 +2,13 @@
     <div>
         <header>🌐 Breedgestone</header>
         <main>
+            <Navbar />
             <router-view />
         </main>
         <footer>© 2025 Breedgestone</footer>
     </div>
 </template>
+<script setup>
+import Navbar from '@/components/Layout/Navbar.vue';
+
+</script>
