cf5de09906a6ad9295b9da6b15946d71c11c5a00
Mavdavis <ekwesimarvelous@gmail.com>
2025-09-18 22:53:53 +0100

    wip

--- src/views/Breedgestone/HomeView.vue
+++ src/views/Breedgestone/HomeView.vue
@@ -4,9 +4,8 @@
     <section class="bg-[#F7F7FD]">
       <div
         class="bg-[#F7F7FD] md:py-16 py-8 max-w-[1920px] mx-auto min-h-[70vh] bg-center bg-no-repeat relative lg:bg-cover lg:bg-[url('/hero.png')] sm:bg-[url('/mobile-hero.png')]">
-
         <div class="relative z-10 flex items-center px-4 md:px-12 max-w-7xl">
-          <div class="2xl:w-[570px] xl:w-[450px] lg:w-[300px] ">
+          <div class="2xl:w-[570px] xl:w-[450px] lg:w-[300px]">
             <h1 class="text-2xl font-campton text-neutral-5 lg:text-3xl xl:text-5xl font-bold leading-tight">
               Africa’s Trusted Hub for Living and Building
             </h1>
@@ -26,7 +25,7 @@
             </div>
           </div>
         </div>
-        <div class="sm:w-3/5 w-full min-w-fit px-4 md:px-12 max-w-7xl ">
+        <div class="sm:w-3/5 w-full min-w-fit px-4 md:px-12 max-w-7xl">
           <!-- Tabs -->
           <div
             class="flex justify-around bg-primary-0 rounded-t-lg gap-4 px-3 pt-3 border-b border-primary-1 sm:w-fit w-full">
@@ -34,7 +33,7 @@
               ' p-2 text-sm font-semibold relative transition-colors',
               activeTab === tab
                 ? 'text-primary-5  border-b-2 border-primary-5  rounded-t-2xl'
-                : 'text-neutral-5 hover:text-neutral-9'
+                : 'text-neutral-5 hover:text-neutral-9',
             ]">
               {{ tab }}
             </button>
@@ -42,30 +41,26 @@
 
           <!-- Form -->
           <div class="p-5 rounded-lg bg-primary-0 sm:block hidden">
-            <div class="flex flex-col sm:flex-row gap-6  items-end">
+            <div class="flex flex-col sm:flex-row gap-6 items-end">
               <!-- Location -->
               <div class="flex-1 flex flex-col">
-                <label class="text-sm  text-neutral-7 mb-2">Location</label>
+                <label class="text-sm text-neutral-7 mb-2">Location</label>
                 <input v-model="location" type="text" placeholder="Enter your Location"
-                  class=" border-0 border-neutral-3 font-semibold text-sm text-neutral-9 placeholder-neutral-4 focus:outline-none focus:border-color-primary-5" />
+                  class="border-0 border-neutral-3 font-semibold text-sm text-neutral-9 placeholder-neutral-4 focus:outline-none focus:border-color-primary-5" />
               </div>
 
               <!-- Date Picker -->
-              <div class="flex-1 flex  flex-col relative">
+              <div class="flex-1 flex flex-col relative">
                 <label class="text-sm text-neutral-7 mb-2">When</label>
                 <input v-model="moveInDate" type="date"
-                  class=" border-0 font-semibold border-neutral-3 text-sm text-neutral-9 focus:outline-none focus:border-color-primary-5 relative" />
-
-
+                  class="border-0 font-semibold border-neutral-3 text-sm text-neutral-9 focus:outline-none focus:border-color-primary-5 relative" />
               </div>
-              <B-button class="flex-1 flex  flex-col relative" @click="handleSearch" size="lg"> Browse
-                Properties
+              <B-button class="flex-1 flex flex-col relative" @click="handleSearch" size="lg">
+                Browse Properties
               </B-button>
             </div>
 
             <!-- Browse Button -->
-
-
           </div>
         </div>
       </div>
@@ -97,19 +92,58 @@
     </section>
 
     <!-- Services -->
-    <section class="py-16 bg-neutral-9 text-white">
-      <div class="max-w-7xl mx-auto px-6 grid md:grid-cols-3 gap-8 text-center">
-        <div class="bg-neutral-8 p-6 rounded-lg">
-          <h3 class="font-bold">Tenants</h3>
-          <p class="mt-2 text-neutral-3">Service description for tenants.</p>
+    <section class="py-16 bg-neutral-8">
+      <div class="max-w-7xl mx-auto px-6 text-primary-0">
+        <img src="../../assets/svg/three-yellow-stars.svg" alt="breedgestone three yellow stars" />
+        <div class="flex md:flex-row flex-col w-full justify-between gap-3 mt-8">
+          <h3 class="font-bold max-w-[700px] font-campton lg:text-3xl xl:text-5xl text-2xl">
+            We Make it Easy for <span class="text-primary-5">TENANTS</span> and
+            <span class="text-primary-5">LANDLORDS</span>
+          </h3>
+          <p class=" max-w-[406px]">
+            Whether you’re looking to rent a cozy shortlet, or purchase your dream home, we simplify
+            the process for you. The best part? You’ll save both time and money with our expert
+            services.
+          </p>
         </div>
-        <div class="bg-neutral-8 p-6 rounded-lg">
-          <h3 class="font-bold">Landlords</h3>
-          <p class="mt-2 text-neutral-3">Service description for landlords.</p>
+        <div class="grid md:grid-cols-3 gap-8 mt-10">
+
+          <div class="flex items-start gap-2 bg-primary-5 p-6 rounded-lg">
+            <img src="../../assets/svg/media-whitebg.svg" alt="">
+            <div class="">
+              <h3 class="font-bold">Tenants</h3>
+              <p class="mt-2 text-neutral-3">Service description for tenants.</p>
+            </div>
+          </div>
+          <div class="flex items-start gap-2 bg-primary-0 text-neutral-9 p-6 rounded-lg">
+            <img src="../../assets/svg/purple-home.svg" alt="">
+            <div class="">
+              <h3 class="font-bold">Landlords</h3>
+              <p class="mt-2 text-neutral-3">Service description for landlords.</p>
+            </div>
+          </div>
+          <div class="flex items-start gap-2 bg-primary-5 p-6 rounded-lg">
+            <img src="../../assets/svg/getReady.svg" alt="">
+            <div class="">
+              <h3 class="font-bold">Builders</h3>
+              <p class="mt-2 text-neutral-3">Service description for builders.</p>
+            </div>
+          </div>
         </div>
-        <div class="bg-neutral-8 p-6 rounded-lg">
-          <h3 class="font-bold">Builders</h3>
-          <p class="mt-2 text-neutral-3">Service description for builders.</p>
+      </div>
+      <div class="border border-[#312F4B] w-full my-8"></div>
+      <div class="w-full text-primary-0 sm:gap-8 gap-4 flex justify-center items-center">
+        <div class="flex flex-col">
+          <h4>7.4%</h4>
+          <p>Property Return Rate</p>
+        </div>
+        <div class="flex flex-col">
+          <h4>7.4%</h4>
+          <p>Property Return Rate</p>
+        </div>
+        <div class="flex flex-col">
+          <h4>7.4%</h4>
+          <p>Property Return Rate</p>
         </div>
       </div>
     </section>
@@ -221,7 +255,7 @@
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref } from 'vue'
 
 import { Navigation, Scrollbar, A11y, Autoplay } from 'swiper/modules'
 
@@ -234,12 +268,11 @@ const onSlideChange = () => {
   console.log('slide change')
 }
 
+const activeTab = ref('Purchase')
+const tabs = ['Purchase', 'Rent', 'Shortlet']
 
-const activeTab = ref("Purchase")
-const tabs = ["Purchase", "Rent", "Shortlet"]
-
-const location = ref("")
-const moveInDate = ref("")
+const location = ref('')
+const moveInDate = ref('')
 
 const handleSearch = () => {
   const searchData = {
@@ -248,10 +281,10 @@ const handleSearch = () => {
     moveInDate: moveInDate.value,
   }
 
-  console.log("Search data:", searchData)
+  console.log('Search data:', searchData)
 
   if (!location.value) {
-    alert("Please enter a location")
+    alert('Please enter a location')
     return
   }
 
